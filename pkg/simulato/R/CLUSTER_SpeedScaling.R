61c5bde7aa5524c68a0699c9abcb472f09d7160b
ar0 (Alexander Rumyantsev) <ar0@krc.karelia.ru>
2021-12-13 14:48:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/simulato@4 dbe4ec64-95df-4436-8523-c444c252a312

--- pkg/simulato/R/CLUSTER_SpeedScaling.R
+++ pkg/simulato/R/CLUSTER_SpeedScaling.R
@@ -1,11 +1,16 @@
 # M/M/N-type Cluster model
-lambda=1
-mu=1
-N=2
-p=c(.5,.5)
-#psw=0.01
-speed=c(0.9,1.2)
-#p=c(.3,.2,.3,.2)
+lambda=0.004556832
+mu=4.801795000000E-08
+N=8
+p=rep(1/8,8)
+pA=0#1
+pD=1#0.8062846
+speed=c(144531.85762576,313327.619175194)
+
+
+is_regeneration <- function(sp){
+  sum(sp$state-c(rep(0,N+2),1))==0
+}
 
 # state: 1:N - number of servers requested by ith oldest customer being served (if any), or zero; N+1: servers requested by first customer in the queue; N+2: queue size (if any); N+3: speed
 # clock: if i=1:N is served, then time of service, N+1: time to arrival 
@@ -23,7 +28,7 @@ update_state <- function(sp,events){   # here we need to switch the state based 
       if(sum(s[1:N])+customer_class<=N) s[which.min(s[1:N])]=customer_class # to servers, if available
       else s[N+1]=customer_class # to queue otherwise  
     }
-    s[N+3]=ifelse(runif(1)<=psw,2,s[N+3])
+    s[N+3]=ifelse(runif(1)<=pA,2,s[N+3])
   }
   else{ # departure
     s[events]=0
@@ -39,7 +44,7 @@ update_state <- function(sp,events){   # here we need to switch the state based 
         s[N+2] <- s[N+2]-1
       }
     }
-    s[N+3]=ifelse(runif(1)<=psw,1,s[N+3])
+    s[N+3]=ifelse(runif(1)<=pD,1,s[N+3])
   }
   return(s)
 }
@@ -58,5 +63,6 @@ start_clocks <- function(sp, new_events) {# here we need to process the clocks b
 }
 performance <- function(sp) {# what is the model performance measured at given point
   #(sum(sp$state[1:(N+1)]>0)+sp$state[N+2]==0) # probabilities
-  sum(sp$state[1:(N+2)])==0
+  #sum(sp$state[1:(N+2)])==0 # prob of idle
+  sum(sp$state[1:(N+1)]>0)+sp$state[N+2]
 }
--- pkg/simulato/R/SIMULATO.R
+++ pkg/simulato/R/SIMULATO.R
@@ -1,12 +1,32 @@
 rm(list=ls())
 tol=1e-8 # machine epsilon, or tolerance in double comparison
 
+# TQ - accumulated performance for cycles; TC - length of cycles; alpha - significance level
+reg_est=function(TQ,TC,alpha){
+  Y=TC$RC*0 # here we accumulate the times for indicators of events, e.g. the queue size equals i
+  Y[TQ$RC[TQ$Q==i]]=TQ$x[TQ$Q==i] # to those cycles only where these happened
+  Z=TC$x # cycle lengths
+  n=length(Z) 
+  est=sum(Y)/sum(Z) # point estimate
+  sq11=sum(Y^2)/(n-1)-sum(Y)^2/(n*(n-1)) # variance of Y
+  sq22=sum(Z^2)/(n-1)-sum(Z)^2/(n*(n-1)) # variance of Z
+  sq12=sum(Y*Z)/(n-1)-sum(Y)*sum(Z)/(n*(n-1)) # covariance of YZ
+  s2=sq11-2*est*sq12+est^2*sq22 # variance esitmate for Y-est*Z
+  s=sqrt(s2) # squared deviation of the same
+  EZ=mean(Z) # mean regenerative period length
+  Kalpha=qnorm(1-alpha/2) # normal distribution quantile
+  lbound=est-Kalpha*s/(EZ*sqrt(n)) # confidence interval
+  rbound=est+Kalpha*s/(EZ*sqrt(n))
+  return(c(lbound,est,rbound))
+}
+
 trace <- function(num_steps=1e5, stop_time=Inf, # default values: 10^5 iterations, unlimited modeling time
                   simpoint_init, # initial state for simulation
                   update_state, # state updating function
                   active_events, # obtaining active events for a state
                   start_clocks, # clock intializing function
                   update_rates, # rate updating function
+                  is_regeneration, # regeneration checking condition
                   performance) { # performance calculating function per point
   perf_ta <- sim_t <- sim_n <- 0
   n_sp <- sp <- simpoint_init()
@@ -19,6 +39,9 @@ trace <- function(num_steps=1e5, stop_time=Inf, # default values: 10^5 iteration
     # here we also need to grab some statistics
     # regenerative estimation here?: TODO
     perf_ta <- perf_ta+performance(sp)*dt # accumulating time average statistics
+     
+    if(is_regeneration(sp)) print("WOW")
+    else print("FOO")
     # performing the transition and updating the point
     sim_t <- sim_t+dt                     # updating the time
     sim_n <- sim_n+1                      # updating point number
@@ -42,6 +65,9 @@ trace <- function(num_steps=1e5, stop_time=Inf, # default values: 10^5 iteration
   }
 
   perf_ta <- perf_ta / sim_t
+  
+  # call reg_est here
+  
   return(list(perf_ta=perf_ta))
 }
 
@@ -49,16 +75,8 @@ trace <- function(num_steps=1e5, stop_time=Inf, # default values: 10^5 iteration
 ############################################################################ BELOW HAS TO BE SET UP FOR EACH MODEL
 
 #setwd("~/Desktop/R/SIMULATO/")
-setwd("/home/ar0/Seafile/My Library/R/SIMULATO")
+#setwd("/home/ar0/Seafile/My Library/R/SIMULATO")
 
 source("CLUSTER_SpeedScaling.R")
 
-res2=rep(0,10)
-for(i in 1:10)
-{
-  psw=i*10/100
-  res2[i]=trace(num_steps = 1000000, stop_time = Inf, simpoint_init,update_state,active_events, start_clocks,update_rates,performance)
-}
-#t1=trace(num_steps = 1000000, stop_time = Inf, simpoint_init,update_state,active_events, start_clocks,update_rates,performance)
-#t1
-plot(seq(0.1,1,0.1),res2[1:10])
+(trace(num_steps = 1000, stop_time = Inf, simpoint_init,update_state,active_events, start_clocks,update_rates,is_regeneration,performance))
